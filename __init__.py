563dabd91602c4d5aa17b41e6f8466bbec1fcf51
Ac0rn <a.aaron.cornelius@gmail.com>
2021-12-07 13:06:30 -0500

    disabled plugin browser and made structs work in dev version

--- __init__.py
+++ __init__.py
@@ -1,23 +1,29 @@
 import os
-import requests
-import shutil
-from zipfile import ZipFile
-from tempfile import TemporaryDirectory
-from PySide2.QtWidgets import (QPushButton, QWidget, QVBoxLayout,
-    QHBoxLayout, QDialog, QFileSystemModel, QTreeView, QLabel, QSplitter,
-    QMessageBox, QHeaderView)
-from PySide2.QtCore import Qt, QFileInfo, QUrl
-
-from PySide2.QtGui import QDesktopServices, QKeySequence
-from PySide2.QtWebEngineWidgets import QWebEnginePage, QWebEngineView, QWebEngineProfile
+#import requests
+#import shutil
+#from zipfile import ZipFile
+#from tempfile import TemporaryDirectory
+#from PySide2.QtWidgets import (QPushButton, QWidget, QVBoxLayout,
+#    QHBoxLayout, QDialog, QFileSystemModel, QTreeView, QLabel, QSplitter,
+#    QMessageBox, QHeaderView)
+#from PySide2.QtCore import Qt, QFileInfo, QUrl
+#
+#from PySide2.QtGui import QDesktopServices, QKeySequence
+#from PySide2.QtWebEngineWidgets import QWebEnginePage, QWebEngineView, QWebEngineProfile
 
 from binaryninja import user_plugin_path
 from binaryninja.log import log_error, log_debug, log_info
-from binaryninja.types import Type, Symbol, Structure
 from binaryninja.plugin import PluginCommand
 from binaryninja.enums import SegmentFlag, SectionSemantics, SymbolType
 from binaryninja.interaction import get_open_filename_input, show_message_box
 from .svdmmap import parse
+
+try:
+    from binaryninja.types import Type, Symbol, StructureBuilder, StructureVariant
+    post3 = True
+except ImportError:
+    from binaryninja.types import Type, Symbol, Structure, StructureType
+    post3 = False
 
 
 svdPath = os.path.realpath(os.path.join(user_plugin_path(), "..", "svd"))
@@ -27,165 +33,165 @@ try:
 except IOError:
     log_error(f"SVD Browser: Unable to create {svdPath}")
 
-class SVDBrowser(QDialog):
-    def __init__(self, context, parent=None):
-        super(SVDBrowser, self).__init__(parent)
-        QWebEngineProfile.defaultProfile().downloadRequested.connect(self.on_downloadRequested)
-
-        # Create widgets
-        #self.setWindowModality(Qt.ApplicationModal)
-        self.title = QLabel(self.tr("SVD Browser"))
-        self.closeButton = QPushButton(self.tr("Close"))
-        self.setWindowTitle(self.title.text())
-        self.browseButton = QPushButton("Browse SVD Folder")
-        self.deleteSvdButton = QPushButton("Delete")
-        self.applySvdButton = QPushButton("Apply SVD")
-        self.view = QWebEngineView()
-        url = "https://developer.arm.com/tools-and-software/embedded/cmsis/cmsis-search"
-        self.view.load(QUrl(url))
-        self.columns = 3
-        self.context = context
-
-        self.currentFileLabel = QLabel()
-        self.currentFile = ""
-
-        #Files
-        self.files = QFileSystemModel()
-        self.files.setRootPath(svdPath)
-        self.files.setNameFilters(["*.svd", "*.patched"])
-
-        #Tree
-        self.tree = QTreeView()
-        self.tree.setModel(self.files)
-        self.tree.setSortingEnabled(True)
-        self.tree.hideColumn(2)
-        self.tree.sortByColumn(0, Qt.AscendingOrder)
-        self.tree.setRootIndex(self.files.index(svdPath))
-        for x in range(self.columns):
-            #self.tree.resizeColumnToContents(x)
-            self.tree.header().setSectionResizeMode(x, QHeaderView.ResizeToContents)
-        treeLayout = QVBoxLayout()
-        treeLayout.addWidget(self.tree)
-        treeButtons = QHBoxLayout()
-        #treeButtons.addWidget(self.newFolderButton)
-        treeButtons.addWidget(self.browseButton)
-        treeButtons.addWidget(self.applySvdButton)
-        treeButtons.addWidget(self.deleteSvdButton)
-        treeLayout.addLayout(treeButtons)
-        treeWidget = QWidget()
-        treeWidget.setLayout(treeLayout)
-
-        # Create layout and add widgets
-        buttons = QHBoxLayout()
-        buttons.addWidget(self.closeButton)
-
-        vlayoutWidget = QWidget()
-        vlayout = QVBoxLayout()
-        vlayout.addWidget(self.view)
-        vlayout.addLayout(buttons)
-        vlayoutWidget.setLayout(vlayout)
-
-        hsplitter = QSplitter()
-        hsplitter.addWidget(treeWidget)
-        hsplitter.addWidget(vlayoutWidget)
-
-        hlayout = QHBoxLayout()
-        hlayout.addWidget(hsplitter)
-
-        self.showMaximized() #Fixes bug that maximized windows are "stuck"
-        #Because you can't trust QT to do the right thing here
-
-        # Set dialog layout
-        self.setLayout(hlayout)
-
-        # Add signals
-        self.closeButton.clicked.connect(self.close)
-        self.tree.selectionModel().selectionChanged.connect(self.selectFile)
-        self.applySvdButton.clicked.connect(self.applySvd)
-        self.deleteSvdButton.clicked.connect(self.deleteSvd)
-        self.browseButton.clicked.connect(self.browseSvd)
-
-    def browseSvd(self):
-        url = QUrl.fromLocalFile(svdPath)
-        QDesktopServices.openUrl(url)
-
-    def selectFile(self, new, old):
-        if len(new.indexes()) == 0:
-            self.tree.clearSelection()
-            self.currentFile = ""
-            return
-        newSelection = self.files.filePath(new.indexes()[0])
-        if QFileInfo(newSelection).isDir():
-            self.tree.clearSelection()
-            self.currentFile = ""
-            return
-        self.currentFile = newSelection
-
-    def applySvd(self):
-        selection = self.tree.selectedIndexes()[::self.columns][0] #treeview returns each selected element in the row
-        svdName = self.files.fileName(selection)
-        if (svdName != ""):
-            question = QMessageBox.question(self, self.tr("Confirm"), self.tr(f"Confirm applying {svdName} to {os.path.basename(self.context.file.filename)} : "))
-            if (question == QMessageBox.StandardButton.Yes):
-                log_debug("SVD Browser: Applying SVD %s." % svdName)
-                load_svd(self.context, self.currentFile)
-                self.close()
-
-    def deleteSvd(self):
-        selection = self.tree.selectedIndexes()[::self.columns][0] #treeview returns each selected element in the row
-        svdName = self.files.fileName(selection)
-        question = QMessageBox.question(self, self.tr("Confirm"), self.tr("Confirm deletion: ") + svdName)
-        if (question == QMessageBox.StandardButton.Yes):
-            log_debug("SVD Browser: Deleting SVD %s." % svdName)
-            self.files.remove(selection)
-            self.tree.clearSelection()
-
-    def on_downloadRequested(self, download):
-        old_path = download.url().path()  # download.path()
-        suffix = QFileInfo(old_path).suffix()
-        if (suffix.lower() in ["zip", "svd", "pack", "patched"]):
-            log_debug(f"SVD Browser: Downloading {str(download.url())}")
-            if suffix.lower() == "svd" or suffix.lower() == "patched":
-                download.setDownloadDirectory(svdPath)
-                download.accept()
-            else:
-                with TemporaryDirectory() as tempfolder:
-                    log_debug(f"SVD Browser: Downloading pack/zip to {tempfolder}")
-                    fname = download.url().fileName()
-                    r = requests.get(download.url().toString(), allow_redirects=True)
-                    dlfile = os.path.join(tempfolder, fname)
-                    open(dlfile, "wb").write(r.content)
-                    '''
-                    # TODO: See if the original QT Downloader can be fixed since it would
-                    # help with situations where the user entered credentials in the browser.
-                    download.setDownloadDirectory(tempfolder)
-                    download.accept()
-                    while not download.finished:
-                        import time
-                        time.sleep(100)
-                    '''
-                    if fname.endswith(".zip") or fname.endswith(".pack"):
-                        destFolder = os.path.join(svdPath, os.path.splitext(fname)[0])
-                        log_debug(f"SVD Browser: Creating {destFolder}")
-                        if not os.path.exists(destFolder):
-                            os.mkdir(destFolder)
-                        with ZipFile(dlfile, 'r') as zipp:
-                            for ifname in zipp.namelist():
-                                if ifname.endswith(".svd"):
-                                    info = zipp.getinfo(ifname)
-                                    info.filename = os.path.basename(info.filename)
-                                    log_debug(f"SVD Browser: Extracting {info.filename} from {ifname}")
-                                    zipp.extract(info, path=destFolder)
-                    else:
-                        #Move file into place
-                        shutil.move(dlfile, svdPath)
-        else:
-            show_message_box("Invalid file", "That download does not appear to be a valid SVD/ZIP/PACK file.")
-        download.cancel()
-
-def launch_browser(bv):
-    svd = SVDBrowser(bv)
-    svd.exec_()
+#class SVDBrowser(QDialog):
+#    def __init__(self, context, parent=None):
+#        super(SVDBrowser, self).__init__(parent)
+#        QWebEngineProfile.defaultProfile().downloadRequested.connect(self.on_downloadRequested)
+#
+#        # Create widgets
+#        #self.setWindowModality(Qt.ApplicationModal)
+#        self.title = QLabel(self.tr("SVD Browser"))
+#        self.closeButton = QPushButton(self.tr("Close"))
+#        self.setWindowTitle(self.title.text())
+#        self.browseButton = QPushButton("Browse SVD Folder")
+#        self.deleteSvdButton = QPushButton("Delete")
+#        self.applySvdButton = QPushButton("Apply SVD")
+#        self.view = QWebEngineView()
+#        url = "https://developer.arm.com/tools-and-software/embedded/cmsis/cmsis-search"
+#        self.view.load(QUrl(url))
+#        self.columns = 3
+#        self.context = context
+#
+#        self.currentFileLabel = QLabel()
+#        self.currentFile = ""
+#
+#        #Files
+#        self.files = QFileSystemModel()
+#        self.files.setRootPath(svdPath)
+#        self.files.setNameFilters(["*.svd", "*.patched"])
+#
+#        #Tree
+#        self.tree = QTreeView()
+#        self.tree.setModel(self.files)
+#        self.tree.setSortingEnabled(True)
+#        self.tree.hideColumn(2)
+#        self.tree.sortByColumn(0, Qt.AscendingOrder)
+#        self.tree.setRootIndex(self.files.index(svdPath))
+#        for x in range(self.columns):
+#            #self.tree.resizeColumnToContents(x)
+#            self.tree.header().setSectionResizeMode(x, QHeaderView.ResizeToContents)
+#        treeLayout = QVBoxLayout()
+#        treeLayout.addWidget(self.tree)
+#        treeButtons = QHBoxLayout()
+#        #treeButtons.addWidget(self.newFolderButton)
+#        treeButtons.addWidget(self.browseButton)
+#        treeButtons.addWidget(self.applySvdButton)
+#        treeButtons.addWidget(self.deleteSvdButton)
+#        treeLayout.addLayout(treeButtons)
+#        treeWidget = QWidget()
+#        treeWidget.setLayout(treeLayout)
+#
+#        # Create layout and add widgets
+#        buttons = QHBoxLayout()
+#        buttons.addWidget(self.closeButton)
+#
+#        vlayoutWidget = QWidget()
+#        vlayout = QVBoxLayout()
+#        vlayout.addWidget(self.view)
+#        vlayout.addLayout(buttons)
+#        vlayoutWidget.setLayout(vlayout)
+#
+#        hsplitter = QSplitter()
+#        hsplitter.addWidget(treeWidget)
+#        hsplitter.addWidget(vlayoutWidget)
+#
+#        hlayout = QHBoxLayout()
+#        hlayout.addWidget(hsplitter)
+#
+#        self.showMaximized() #Fixes bug that maximized windows are "stuck"
+#        #Because you can't trust QT to do the right thing here
+#
+#        # Set dialog layout
+#        self.setLayout(hlayout)
+#
+#        # Add signals
+#        self.closeButton.clicked.connect(self.close)
+#        self.tree.selectionModel().selectionChanged.connect(self.selectFile)
+#        self.applySvdButton.clicked.connect(self.applySvd)
+#        self.deleteSvdButton.clicked.connect(self.deleteSvd)
+#        self.browseButton.clicked.connect(self.browseSvd)
+#
+#    def browseSvd(self):
+#        url = QUrl.fromLocalFile(svdPath)
+#        QDesktopServices.openUrl(url)
+#
+#    def selectFile(self, new, old):
+#        if len(new.indexes()) == 0:
+#            self.tree.clearSelection()
+#            self.currentFile = ""
+#            return
+#        newSelection = self.files.filePath(new.indexes()[0])
+#        if QFileInfo(newSelection).isDir():
+#            self.tree.clearSelection()
+#            self.currentFile = ""
+#            return
+#        self.currentFile = newSelection
+#
+#    def applySvd(self):
+#        selection = self.tree.selectedIndexes()[::self.columns][0] #treeview returns each selected element in the row
+#        svdName = self.files.fileName(selection)
+#        if (svdName != ""):
+#            question = QMessageBox.question(self, self.tr("Confirm"), self.tr(f"Confirm applying {svdName} to {os.path.basename(self.context.file.filename)} : "))
+#            if (question == QMessageBox.StandardButton.Yes):
+#                log_debug("SVD Browser: Applying SVD %s." % svdName)
+#                load_svd(self.context, self.currentFile)
+#                self.close()
+#
+#    def deleteSvd(self):
+#        selection = self.tree.selectedIndexes()[::self.columns][0] #treeview returns each selected element in the row
+#        svdName = self.files.fileName(selection)
+#        question = QMessageBox.question(self, self.tr("Confirm"), self.tr("Confirm deletion: ") + svdName)
+#        if (question == QMessageBox.StandardButton.Yes):
+#            log_debug("SVD Browser: Deleting SVD %s." % svdName)
+#            self.files.remove(selection)
+#            self.tree.clearSelection()
+#
+#    def on_downloadRequested(self, download):
+#        old_path = download.url().path()  # download.path()
+#        suffix = QFileInfo(old_path).suffix()
+#        if (suffix.lower() in ["zip", "svd", "pack", "patched"]):
+#            log_debug(f"SVD Browser: Downloading {str(download.url())}")
+#            if suffix.lower() == "svd" or suffix.lower() == "patched":
+#                download.setDownloadDirectory(svdPath)
+#                download.accept()
+#            else:
+#                with TemporaryDirectory() as tempfolder:
+#                    log_debug(f"SVD Browser: Downloading pack/zip to {tempfolder}")
+#                    fname = download.url().fileName()
+#                    r = requests.get(download.url().toString(), allow_redirects=True)
+#                    dlfile = os.path.join(tempfolder, fname)
+#                    open(dlfile, "wb").write(r.content)
+#                    '''
+#                    # TODO: See if the original QT Downloader can be fixed since it would
+#                    # help with situations where the user entered credentials in the browser.
+#                    download.setDownloadDirectory(tempfolder)
+#                    download.accept()
+#                    while not download.finished:
+#                        import time
+#                        time.sleep(100)
+#                    '''
+#                    if fname.endswith(".zip") or fname.endswith(".pack"):
+#                        destFolder = os.path.join(svdPath, os.path.splitext(fname)[0])
+#                        log_debug(f"SVD Browser: Creating {destFolder}")
+#                        if not os.path.exists(destFolder):
+#                            os.mkdir(destFolder)
+#                        with ZipFile(dlfile, 'r') as zipp:
+#                            for ifname in zipp.namelist():
+#                                if ifname.endswith(".svd"):
+#                                    info = zipp.getinfo(ifname)
+#                                    info.filename = os.path.basename(info.filename)
+#                                    log_debug(f"SVD Browser: Extracting {info.filename} from {ifname}")
+#                                    zipp.extract(info, path=destFolder)
+#                    else:
+#                        #Move file into place
+#                        shutil.move(dlfile, svdPath)
+#        else:
+#            show_message_box("Invalid file", "That download does not appear to be a valid SVD/ZIP/PACK file.")
+#        download.cancel()
+#
+#def launch_browser(bv):
+#    svd = SVDBrowser(bv)
+#    svd.exec_()
 
 def load_svd(bv, svd_file = None):
     if not svd_file:
@@ -210,7 +216,11 @@ def load_svd(bv, svd_file = None):
         bv.define_user_symbol(Symbol(SymbolType.ImportedDataSymbol, p['base'], p['name']))
 
     for p in base_peripherals:
-        s = Structure()
+        if post3:
+            s = StructureBuilder.create(type=StructureVariant.StructStructureType)
+        else:
+            s = Structure()
+            s.type = StructureType.StructStructureType
         for r in p['registers'].values():
             if r['size'] is None:
                 s.insert(r['offset'], Type.int(4, False), r['name'])
@@ -225,11 +235,11 @@ def load_svd(bv, svd_file = None):
         register_peripheral(p, struct_type)
 
 
-PluginCommand.register(
-    "SVD\\SVD Browser",
-    "Manage SVD files and browse/search for new files to load.",
-    launch_browser
-)
+#PluginCommand.register(
+#    "SVD\\SVD Browser",
+#    "Manage SVD files and browse/search for new files to load.",
+#    launch_browser
+#)
 
 PluginCommand.register(
     "SVD\\Load SVD",
